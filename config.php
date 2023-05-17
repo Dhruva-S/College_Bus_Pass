3c0aafecc4f0a75bb245f47e483dc6375dbf2b0c
Dhruva-S <73852505+Dhruva-S@users.noreply.github.com>
2023-05-17 15:40:56 +0530

    changed database name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bus_booking";
+$dbname = "college_pass";
 
   $con = mysqli_connect($servername,$username,$password,$dbname);
   //check
